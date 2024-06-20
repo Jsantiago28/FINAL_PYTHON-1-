77638f07277e7c3289afd567c47a567eddbbf398
ManuelaJimenez28 <86388886+ManuelaJimenez28@users.noreply.github.com>
2024-06-20 23:47:26 +0200

    Add files via upload
    agregando funciones para filtrar

--- gestor_tareas.py
+++ gestor_tareas.py
@@ -1,29 +1,29 @@
-import os  # Módulo para operaciones del sistema de archivos
-import json  # Módulo para trabajar con JSON
-import random  # Módulo para generar números aleatorios
+import os  #Modulo para operaciones del sistema de archivos
+import json  #Modulo para trabajar con JSON
+import random  #Modulo para generar números aleatorios
 
 class GestorTareas:
     def __init__(self, directorio='tareas'):
-        """ Constructor que inicializa el directorio donde se guardarán las tareas. Si el directorio no existe, lo crea."""
+        """Constructor que inicializa el directorio donde se guardarán las tareas. Si el directorio no existe, crea uno nuevo."""
         self.directorio = directorio
         if not os.path.exists(directorio):
             os.makedirs(directorio)
 
     def _ruta_tarea(self, id_tarea):
-        """ Método privado que genera la ruta del archivo donde se almacenará una tarea específica."""
+        """Metodo para generar la ruta del archivo donde se va a almacenar una tarea"""
         return os.path.join(self.directorio, f'tarea_{id_tarea}.json')
 
     def generar_id_unico(self, tareas):
-        """Genera un ID único de 3 dígitos para una nueva tarea. Comprueba que el ID no exista ya en el diccionario de tareas. """
+        """Genera un ID unico de 3 digitos para una nueva tarea y comprueba que el ID no exista en el diccionario de tareas."""
         while True:
-            id_unico = random.randint(100, 999)  # Genera un número aleatorio entre 100 y 999
+            id_unico = random.randint(100, 999)  #Genera un número aleatorio entre 100 y 999
             if id_unico not in tareas:
                 return id_unico
 
     def crear_tarea(self, descripcion, fecha_limite, prioridad, categoria):
-        """ Crea una nueva tarea con los detalles proporcionados y la guarda en un archivo JSON."""
-        tareas = self.leer_tareas()  # Lee las tareas existentes
-        next_id = self.generar_id_unico(tareas)  # Genera un ID único para la nueva tarea
+        """Crea una nueva tarea con los detalles proporcionados y la guarda en un archivo JSON."""
+        tareas = self.leer_tareas()  #Lee las tareas existentes
+        next_id = self.generar_id_unico(tareas)  #Genera un ID único para la nueva tarea
         tarea = {
             "id": next_id,
             "descripcion": descripcion,
@@ -32,29 +32,28 @@ class GestorTareas:
             "categoria": categoria,
             "estado": "pendiente"
         }
-        ruta = self._ruta_tarea(next_id)  # Genera la ruta del archivo para la nueva tarea
+        ruta = self._ruta_tarea(next_id)  #Genera la ruta del archivo para la nueva tarea
         with open(ruta, 'w') as archivo:
-            json.dump(tarea, archivo)  # Guarda la tarea en un archivo JSON
+            json.dump(tarea, archivo)  #Guarda la tarea en un archivo JSON
         print("Tarea agregada exitosamente.")
 
     def leer_tareas(self):
-        """Lee todas las tareas guardadas y las devuelve en un diccionario."""
+        #Lee todas las tareas guardadas y las devuelve en un diccionario.
         tareas = {}
         for archivo in os.listdir(self.directorio):
             if archivo.endswith('.json'):
                 ruta = os.path.join(self.directorio, archivo)
                 with open(ruta, 'r') as f:
-                    tarea = json.load(f)  # Carga la tarea desde el archivo JSON
-                    tareas[tarea["id"]] = tarea  # Añade la tarea al diccionario de tareas
+                    tarea = json.load(f)  #Carga la tarea desde el archivo JSON
+                    tareas[tarea["id"]] = tarea  #Añade la tarea al diccionario de tareas
         return tareas
 
     def actualizar_tarea(self, id_tarea, descripcion=None, fecha_limite=None, prioridad=None, categoria=None, estado=None):
-        """Actualiza los detalles de una tarea existente."""
-        
-        ruta = self._ruta_tarea(id_tarea)  # Genera la ruta del archivo de la tarea a actualizar
+        #Actualiza los detalles de una tarea existente.
+        ruta = self._ruta_tarea(id_tarea)  #Genera la ruta del archivo de la tarea a actualizar
         if os.path.exists(ruta):
             with open(ruta, 'r') as archivo:
-                tarea = json.load(archivo)  # Carga la tarea desde el archivo JSON
+                tarea = json.load(archivo)  #Carga la tarea desde el archivo JSON
             if descripcion:
                 tarea["descripcion"] = descripcion
             if fecha_limite:
@@ -66,16 +65,23 @@ class GestorTareas:
             if estado:
                 tarea["estado"] = estado
             with open(ruta, 'w') as archivo:
-                json.dump(tarea, archivo)  # Guarda los cambios en el archivo JSON
+                json.dump(tarea, archivo)  #Guarda los cambios en un archivo JSON
             return True
         return False
 
     def eliminar_tarea(self, id_tarea):
-        """
-        Elimina una tarea existente.
-        """
-        ruta = self._ruta_tarea(id_tarea)  # Genera la ruta del archivo de la tarea a eliminar
+        #Elimina una tarea existente con el ID indicado.
+        ruta = self._ruta_tarea(id_tarea)  #Genera la ruta del archivo de la tarea a eliminar
         if os.path.exists(ruta):
-            os.remove(ruta)  # Elimina el archivo de la tarea
+            os.remove(ruta)  #Elimina el archivo de la tarea
             return True
         return False
+
+    def leer_prioridad(self, prioridad):
+        #Retorna una lista de tareas que coinciden con la prioridad dada.
+        return [tarea for tarea in self.leer_tareas().values() if tarea['prioridad'] == prioridad]
+
+    def leer_categoria(self, categoria):
+        #Retorna una lista de tareas que coinciden con la categoría dada.
+        return [tarea for tarea in self.leer_tareas().values() if tarea['categoria'] == categoria]
+
--- main.py
+++ main.py
@@ -1,64 +1,67 @@
-from gestor_tareas import GestorTareas  # Importa la clase GestorTareas del módulo gestor_tareas
+from gestor_tareas import GestorTareas  #Importa la clase GestorTareas del modulo gestor_tareas
 
 def mostrar_menu():
-    """
-    Muestra el menú principal del sistema de gestión de tareas.
-    """
+   
+    #Muestra el manu principal del sistema de gestión de tareas
+   
     print("\nSistema de Gestión de Tareas")
     print("1. Agregar Tarea")
     print("2. Ver Tareas")
     print("3. Actualizar Tarea")
     print("4. Eliminar Tarea")
-    print("5. Salir")
+    print("5. Filtrar Tareas por Prioridad")
+    print("6. Filtrar Tareas por Categoria")
+    print("7. Salir")
 
+#agrega una tarea al sistema
 def agregar_tarea(gestor):
-    """Solicita los detalles de una nueva tarea al usuario y la agrega usando el gestor de tareas."""
-    descripcion = input("Descripción: ")
-    fecha_limite = input("Fecha Límite (YYYY-MM-DD): ")
+    """Solicita los detalles de una nueva taera al usuario y la agrega usando el gestor de tareas."""
+    descripcion = input("Descripcion: ")
+    fecha_limite = input("Fecha Limite (DD/MM/YYYY): ")
     prioridad = input("Prioridad (baja, media, alta): ")
     categoria = input("Categoría: ")
     gestor.crear_tarea(descripcion, fecha_limite, prioridad, categoria)
 
 def ver_tareas(gestor):
-    """ Muestra todas las tareas existentes.Permite al usuario volver al menú principal o salir del programa."""
+    """Muestra todas las tareas existentes. Permite al usuario volver al menú principal o salir del programa."""
     tareas = gestor.leer_tareas()
     if not tareas:
         print("No hay tareas.")
     else:
         for id_tarea, tarea in tareas.items():
-            print(f"ID: {tarea['id']}, Descripción: {tarea['descripcion']}, Fecha Límite: {tarea['fecha_limite']}, Prioridad: {tarea['prioridad']}, Categoría: {tarea['categoria']}, Estado: {tarea['estado']}")
-    print("\n1. Volver al menú principal")
+            print(f"ID: {tarea['id']}, Descripcion: {tarea['descripcion']}, Fecha Limite: {tarea['fecha_limite']}, Prioridad: {tarea['prioridad']}, Categoría: {tarea['categoria']}, Estado: {tarea['estado']}")
+    print("\n1. Volver al menu principal")
     print("2. Salir del programa")
-    opcion = input("Seleccione una opción: ")
+    opcion = input("Seleccione una opcion: ")
     if opcion == "2":
         print("Saliendo del programa.")
         exit()
 
 def actualizar_tarea(gestor):
-    """ Actualiza una tarea existente solicitando los nuevos detalles al usuario. Si no hay tareas, sugiere agregar una tarea primero.
-    Permite al usuario volver al menú principal o salir del programa."""
-    
+    """Actualiza una tarea existente solicitando el ID al usuario y si no hay tareas, pide agregar una tarea primero."""
+   
     tareas = gestor.leer_tareas()
     if not tareas:
         print("No hay tareas. Agregue una tarea primero.")
         return
 
-    id_tarea = int(input("ID de la tarea: "))
+    id_tarea = int(input("Ingrese el ID de la tarea: "))
     if id_tarea not in tareas:
-        print("ID no encontrado.")
+        print("El ID ingresado no fue encontrado.")
         return
 
-    descripcion = input("Nueva Descripción: ")
-    fecha_limite = input("Nueva Fecha Límite (YYYY-MM-DD): ")
-    prioridad = input("Nueva Prioridad (baja, media, alta): ")
-    categoria = input("Nueva Categoría: ")
-    estado = input("Nuevo Estado (pendiente, en progreso, completada): ")
+    descripcion = input("Nueva descripcion, si desea dejarlo igual presione Enter:: ")
+    fecha_limite = input("Nueva fecha limite (DD/MM/YYYY), Si desea dejarlo igual presione Enter:: ")
+    prioridad = input("Nueva prioridad (baja, media, alta), si desea dejarlo igual presione Enter: ")
+    categoria = input("Nueva categoría, si desea dejarla igual presione Enter:: ")
+    estado = input("Nuevo estado (pendiente, en progreso, completada), si desea dejarlo igual presione Enter:: ")
+   
     if gestor.actualizar_tarea(id_tarea, descripcion=descripcion, fecha_limite=fecha_limite, prioridad=prioridad, categoria=categoria, estado=estado):
         print("Tarea actualizada exitosamente.")
     else:
         print("Tarea no encontrada.")
 
-    print("\n1. Volver al menú principal")
+    print("\n1. Volver al menu principal")
     print("2. Salir del programa")
     opcion = input("Seleccione una opción: ")
     if opcion == "2":
@@ -66,19 +69,37 @@ def actualizar_tarea(gestor):
         exit()
 
 def eliminar_tarea(gestor):
-    """
-    Elimina una tarea existente solicitando su ID al usuario.
-    """
-    id_tarea = int(input("ID de la tarea: "))
+    #Elimina una tarea existente solicitando su ID al usuario.
+   
+    id_tarea = int(input("Ingrese el ID de la tarea: "))
     if gestor.eliminar_tarea(id_tarea):
         print("Tarea eliminada exitosamente.")
     else:
         print("Tarea no encontrada.")
 
+def filtrar_prioridad(gestor):
+    """Filtra las tareas por prioridad solicitando al usuario la prioridad deseada."""
+    prioridad = input("Ingrese la prioridad por la que desea filtrar (baja, media, alta): ").lower()
+    tareas = gestor.leer_prioridad(prioridad)
+    if not tareas:
+        print(f"No hay tareas con la prioridad {prioridad}.")
+    else:
+        for tarea in tareas:
+            print(f"ID: {tarea['id']}, Descripcion: {tarea['descripcion']}, Fecha Limite: {tarea['fecha_limite']}, Prioridad: {tarea['prioridad']}, Categoría: {tarea['categoria']}, Estado: {tarea['estado']}")
+
+def filtrar_categoria(gestor):
+    """Filtra las tareas por categoria solicitando al usuario la categoría deseada."""
+    categoria = input("Ingrese la categoria para filtrar: ").lower()
+    tareas = gestor.leer_categoria(categoria)
+    if not tareas:
+        print(f"No hay tareas en la categoria {categoria}.")
+    else:
+        for tarea in tareas:
+            print(f"ID: {tarea['id']}, Descripcion: {tarea['descripcion']}, Fecha Limite: {tarea['fecha_limite']}, Prioridad: {tarea['prioridad']}, Categoria: {tarea['categoria']}, Estado: {tarea['estado']}")
+
 def main():
-    """
-    Función principal que ejecuta el programa y muestra el menú principal en un bucle.
-    """
+    """Función principal que ejecuta el programa y muestra el menú principal en un ciclo While."""
+   
     gestor = GestorTareas()
     while True:
         mostrar_menu()
@@ -92,6 +113,10 @@ def main():
         elif opcion == "4":
             eliminar_tarea(gestor)
         elif opcion == "5":
+            filtrar_prioridad(gestor)
+        elif opcion == "6":
+            filtrar_categoria(gestor)
+        elif opcion == "7":
             print("Saliendo del sistema de gestión de tareas.")
             break
         else:
